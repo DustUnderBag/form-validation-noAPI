a821b12f459eca6a5811067517e419c9cc564977
DustUnderBag <usamary55@gmail.com>
2025-03-24 14:13:58 -0400

    Complete check minlength method for password
    During input, this function validates the minlength of password and
    report validity message at the same time.

--- src/index.js
+++ src/index.js
@@ -2,3 +2,27 @@ import "./reset.css";
 import "./styles.css";
 
 console.log("Script entry point working");
+
+const email = document.querySelector("#email");
+const country = document.querySelector("#country");
+const postalCode = document.querySelector("#postal-code");
+const pwd = document.querySelector("#pwd");
+const cfmPwd = document.querySelector("#cfm-pwd");
+
+pwd.addEventListener("input", (e) => {
+  checkMinLength(e);
+});
+
+function checkMinLength(e) {
+  const self = e.target;
+  const minlength = Number(self.getAttribute("minlength"));
+
+  if (self.value.length < minlength) {
+    self.setCustomValidity(`Must contain at least ${minlength} characters.`);
+    self.reportValidity();
+    return false;
+  }
+
+  self.setCustomValidity("");
+  return true;
+}
